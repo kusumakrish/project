dae80ab06b5d54164851b1d1340909a00e273b24
kusumakrishna <kusumakrishna524@gmail.com>
2022-07-31 22:40:07 +0530

    change

--- Routes/data.js
+++ Routes/data.js
@@ -2,7 +2,7 @@ const express = require ("express")
 const datacontroller = require("../Controllers/data")
 
 const DataRouter = express.Router()//we are linking our data.js file with our /api main
-DataRouter.route("/details")//creating a route
+DataRouter.route("/")//creating a route
 .get(datacontroller.ApiController)//calling the data.js in Controller folder
 
 module.exports = DataRouter
